6c1a9b9975f17526d8c140637394afa182f1c288
Str <dvl.java@gmail.com>
2016-06-16 00:25:14 +0300

    горизтальный вид и прочие нужности

--- app/src/main/java/com/jexample/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/jexample/android/geoquiz/QuizActivity.java
@@ -1,19 +1,23 @@
 package com.jexample.android.geoquiz;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
 
-import java.util.logging.Logger;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private static final String TAG = "QuizActivity";
+
+    private static final String CURRENT_INDEX = "index";
+
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
 
     private TextView mQuestionTextView;
 
@@ -28,6 +32,8 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, ">> Create");
+
         setContentView(R.layout.activity_quiz);
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
@@ -35,40 +41,108 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkAnsver(true);
+                checkAnswer(true);
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkAnsver(false);
+                checkAnswer(false);
             }
         });
 
-        mNextButton = (Button) findViewById(R.id.next_button);
-        mNextButton.setOnClickListener(new View.OnClickListener() {
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(CURRENT_INDEX, 0);
+        }
+        View.OnClickListener listenerNext = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBlank.length;
-
                 updateQuestion();
             }
-        });
+        };
 
+        mNextButton.setOnClickListener(listenerNext);
+        mQuestionTextView.setOnClickListener(listenerNext);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (mCurrentIndex == 0) {
+                    Toast.makeText(QuizActivity.this, "This is First Question!", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    mCurrentIndex = (mCurrentIndex - 1) % mQuestionBlank.length;
+                    updateQuestion();
+                }
+            }
+        });
 
         // first update
         updateQuestion();
 
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, ">> On Save Inastance <<");
+        outState.putInt(CURRENT_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, ">> On Start");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, ">> Restart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, ">> Stop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, ">> Pause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, ">> Destroyyyy");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, ">> Resume");
+    }
+
     private void updateQuestion() {
-        int questionResId = mQuestionBlank[mCurrentIndex].getTextResId();
+        Question q = null;
+        try {
+            q = mQuestionBlank[mCurrentIndex];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            Log.e(TAG, "Index was out");
+        }
+        int questionResId = q.getTextResId();
         mQuestionTextView.setText(questionResId);
 
     }
 
-    private void checkAnsver(boolean answerUser) {
+    private void checkAnswer(boolean answerUser) {
         Question question = mQuestionBlank[mCurrentIndex];
         int checkAnswerId;
         if (question.isAnswerTrue() == answerUser) {
