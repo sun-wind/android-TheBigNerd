fe4352534d85328ec839dd32cca4b2e51f6183b8
Str <dvl.java@gmail.com>
2016-06-23 00:23:57 +0300

    не зевай! баги!

--- app/src/main/java/com/jexample/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/jexample/android/geoquiz/QuizActivity.java
@@ -9,12 +9,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.*;
 
+import java.util.ArrayList;
+
 
 public class QuizActivity extends AppCompatActivity {
 
     private static final String TAG = "QuizActivity";
 
     private static final String CURRENT_INDEX = "index";
+    private static final String CURRENT_CHEATS = "cheatsIndex";
 
     private static final int REQUEST_CODE_CHEAT = 0;
 
@@ -32,9 +35,11 @@ public class QuizActivity extends AppCompatActivity {
     };
 
     private int mCurrentIndex = 0;
+    @Deprecated
     private boolean isCheater;
+    private ArrayList<Integer> cheatedIndexes = new ArrayList<>();
 
-    @SuppressLint("WrongViewCast")
+   // @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,8 +54,11 @@ public class QuizActivity extends AppCompatActivity {
         mPrevButton = (ImageButton) findViewById(R.id.prev_button);
         mCheatButton = (Button) findViewById(R.id.cheat_button);
 
+        /* restore data from rotate*/
         if (savedInstanceState != null) {
             mCurrentIndex = savedInstanceState.getInt(CURRENT_INDEX, REQUEST_CODE_CHEAT);
+            cheatedIndexes = savedInstanceState.getIntegerArrayList(CURRENT_CHEATS);
+//            isCheater = savedInstanceState.getBoolean(CURRENT_CHEAT, false);
         }
 
         /* listeners */
@@ -71,7 +79,7 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBlank.length;
                 updateQuestion();
-                isCheater = false;
+//                isCheater = false;
             }
         };
 
@@ -93,7 +101,6 @@ public class QuizActivity extends AppCompatActivity {
         mCheatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //
                 boolean answerIsTrue = mQuestionBlank[mCurrentIndex].isAnswerTrue();
                 Intent intent = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
                 startActivityForResult(intent, REQUEST_CODE_CHEAT);
@@ -123,15 +130,24 @@ public class QuizActivity extends AppCompatActivity {
             if (data == null) {
                 return;
             }
-            isCheater = CheatActivity.wasAnswerShown(data);
-            Log.d(TAG, ">> Was Answer Shown: " + isCheater);
+            boolean isCheater = CheatActivity.wasAnswerShown(data);
+            if(isCheater) {
+                cheatedIndexes.add(mCurrentIndex);
+            }
+            //Log.d(TAG, ">> Was Answer Shown: " + isCheater);
         }
     }
 
+    /**
+     * Save data from rotate
+     * @param outState
+     */
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         Log.d(TAG, ">> On Save Instance <<");
+//        outState.putBoolean(CURRENT_CHEAT, isCheater);
+        outState.putIntegerArrayList(CURRENT_CHEATS, cheatedIndexes);
         outState.putInt(CURRENT_INDEX, mCurrentIndex);
     }
 
@@ -185,11 +201,12 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     private void checkAnswer(boolean userPressed) {
-        Question question = mQuestionBlank[mCurrentIndex];
+
         int checkAnswerId;
-        if (isCheater) {
+        if (cheatedIndexes.contains(mCurrentIndex)) {
             checkAnswerId = R.string.judgment_toast;
         } else {
+            Question question = mQuestionBlank[mCurrentIndex];
             if (question.isAnswerTrue() == userPressed) {
                 checkAnswerId = R.string.correct_toast;
             } else {
